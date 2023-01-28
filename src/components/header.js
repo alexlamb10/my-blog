0063954a6fb92c11a45c35813a9a69d0576f91f6
Alex Lamb <alexlamb104@gmail.com>
2023-01-27 23:52:25 -0700

    Half of responsive navbar finished

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import { Link, StaticQuery, graphql } from "gatsby"
 import React from "react"
 import { siteMetadata } from "../../gatsby-config"
 import * as styles from "./header.module.scss"
+import { Bars3Icon } from "@heroicons/react/20/solid"
 
 const HeaderLink = props => (
   <Link className={styles.link} to={props.to}>
@@ -31,12 +32,26 @@ export default () => (
         <div className={styles.home}>
           <HeaderLink to="/" text="ALEX LAMB" />
         </div>
-        <div className={styles.other}>
-          <HeaderLink to="/" text="HOME" />
-          <HeaderLink to="/about" text="RESUME" />
-          {/* <HeaderLink to="/blog-posts" text="BLOG" /> */}
-          <HeaderLink to="/contact" text="CONTACT" />
-        </div>
+        <input type="checkbox" id="checkbox" />
+        <label htmlFor="checkbox" id={styles.icon}>
+          <hr />
+          <hr />
+          <hr />
+        </label>
+        <ul className={styles.other}>
+          <li className={styles.active}>
+            <HeaderLink to="/" text="HOME" />
+          </li>
+          <li>
+            <HeaderLink to="/about" text="RESUME" />
+          </li>
+          <li>
+            <HeaderLink to="/blog-posts" text="BLOG" />
+          </li>
+          <li>
+            <HeaderLink to="/contact" text="CONTACT" />
+          </li>
+        </ul>
       </header>
     )}
   />
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,5 +4,9 @@ import Layout from "../components/layout"
 import Title from "../components/title"
 
 export default function Contact() {
-  return <Layout>This is still a page</Layout>
+  return (
+    <Layout>
+      <Title text="Contact" />
+    </Layout>
+  )
 }
